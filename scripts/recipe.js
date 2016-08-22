703da1555bb340c2400a784e83cff604f2114d6e
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-22 17:05:39 +0800

    modify agax file, add footer in indexfile, add css for indexfile container

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,29 +1,54 @@
+// var urlParam = function(name, w){
+//     w = w || window;
+//     var rx = new RegExp('[\&|\?]'+name+'=([^\&\#]+)'),
+//         val = w.location.search.match(rx);
+//     return !val ? '':val[1];
+// };
+// var useId = urlParam('_id');
+
 console.log("recipe JS connected");
+
 $(function() {
+
 var $recipeImage        = $('#recipeimage');
 var $recipeTitle        = $('#recipe-title');
 var $recipeDirections   = $('#intructions');
 var $recipeIngredients  = $('#ingredient-img');
+var $recipeCalories     = $('#calories');
+
 // AJAX call to get recipe
+
 //Recipe ajax call
-  // $btn3.on('click', function() {
-  //   console.log('click click click');
-    $.ajax({
-      url: 'https://team5-backend.herokuapp.com/API/recipes',
-      type: 'GET',
-      dataType: 'json',
-     }).done(function(data) {
-       $recipeImage.html(
-         $("<img src=\"" + data[0].image_url + "\">"));
-      $recipeTitle.html(
-       $("<p> Title: " + data[0].title + "</p>"));
-      $recipeDirections.html(
-     $("<p> " + data[0].directions + "</p>"));
-      $recipeIngredients.html(
-      $("<p> Ingredients: " + data[0].ingredients + "</p>"));
-    })
-    .fail(function(request, textStatus, errorThrown) {
-      $recipeDirections.html("Sorry! An error occured when processing your phrase. Request " + request.status + " " + textStatus + " " + errorThrown);
-    });
-  });
+ // $btn3.on('click', function() {
+ //   console.log('click click click');
+   $.ajax({
+
+     url: 'https://team5-backend.herokuapp.com/API/recipes/57ba977d5e2c685b25483814',
+     type: 'GET',
+
+     dataType: 'json',
+    }).done(function(data) {
+     $recipeImage.html(
+     $("<img src=\"" + data.image_url + "\">"));
+
+     $recipeTitle.html(
+     $("<p>  " + data.title + "</p>"));
+
+     $recipeDirections.html(
+     $("<p>" + data.directions + "</p>"));
+
+     $recipeIngredients.html(
+     $("<p>" + data.ingredients + "</p>"));
+
+     $recipeCalories.html(
+     $("<p> Calories: " + data.total_calories + "</p>"));
+
+   })
+   .fail(function(request, textStatus, errorThrown) {
+     $recipeDirections.html("Sorry! An error occured when processing your phrase. Request " + request.status + " " + textStatus + " " + errorThrown);
+   });
+ });
+
+
+
 // populate HTML page with recipe data
