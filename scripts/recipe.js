d44e9b9831ccd4343b80a21d43d99c7bd13ce434
Christopher Khoo <khooweiwen@gmail.com>
2016-08-23 16:34:10 +0800

    dunno

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,6 +1,32 @@
 console.log("recipe JS connected");
 
 // AJAX call to get recipe
+$.ajax({
+  url: "https://team5-backend.herokuapp.com/API/recipes",
+  // headers: {'X-Mashape-Key': "eacbvBkAmomshikYXvMKDr0B269ap16OPTRjsn9MD1cVErXCtc",
+  // "Content-Type": "application/x-www-form-urlencoded",
+  // "Accept": "application/json"
+  // },
+  type: "GET",
+  dataType: 'json',
+  beforeSend: function() {
+    $loader.show();
+  }
+}).done(successFunction)
+  .fail(failFunction)
+  .always(alwaysFunction);
+});
 
+function alwaysFunction(){
+  $loader.hide();
+}
+function successFunction(data){
+  $quote.html('<strong>&ldquo;</strong><em> '+ data.quote +' </em><strong>&rdquo;</strong>');
+  $author.html('<small>- ' + data.author + '</small>');
+  $('input#input-search').val(data.author);
+}
+function failFunction(request, textStatus, errorThrown){
+  $quote.text(textStatus + ' occurred during your request: '+ errorThrown );
+}
 
 // populate HTML page with recipe data
