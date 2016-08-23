081eda99809bae2384cda45ab498548c4ea43ed9
xin chen <cxin7@outlook.com>
2016-08-23 15:27:11 +0800

    login and sign up locally

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,16 @@
-console.log("index JS connected");
-// Static page no JS required
+$(function(){
+  $("#logout").on('click',function(){
+    $.ajax({
+      url: "https://team5-backend.herokuapp.com/API/logout",
+      type: 'GET',
+    }).done(function(data) {
+      window.location = 'index.html';
+  })
+  .fail(function(request, textStatus, errorThrown) {
+    alert('An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + errorThrown);
+  });
+});
+
+
+
+});
--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,54 @@
-console.log("login JS connected");
-
 // CX to do
 // provide user_id in sessions for use in rest of app
+$(function(){
+  $("#btn-signup").on('click',function(){
+    var $email = $("#email"),
+    $password = $("#password");
+    $.ajax({
+      url: "https://team5-backend.herokuapp.com/API/signup",
+      data: {
+        "email": $email.val(),
+        "password": $password.val()
+      },
+      type: 'POST',
+      dataType:'json'
+    }).done(function(data) {
+      window.location = 'index.html';
+  })
+  .fail(function(request, textStatus, errorThrown) {
+    alert('An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + errorThrown);
+  });
+});
+
+$("#btn-login").on('click',function(){
+  var $email = $("#login-username"),
+  $password = $("#login-password");
+  $.ajax({
+    url: "https://team5-backend.herokuapp.com/API/login",
+    data: {
+      "email": $email.val(),
+      "password": $password.val()
+    },
+    type: 'POST',
+    dataType:'json'
+  }).done(function(data) {
+    alert(data);
+    window.location = 'index.html';
+})
+.fail(function(request, textStatus, errorThrown) {
+  alert('An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + errorThrown);
+});
+});
+$('#btn-fblogin').on('click',function(){
+  $.ajax({
+    url: "https://crossorigin.me/https://team5-backend.herokuapp.com/API/auth/google",
+    type: 'GET',
+  }).done(function(data) {
+    window.location = 'index.html';
+})
+.fail(function(request, textStatus, errorThrown) {
+  alert('An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + errorThrown);
+});
+});
+
+});
